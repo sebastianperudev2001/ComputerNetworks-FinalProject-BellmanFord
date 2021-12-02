96a377bff77acec1e54c19f44b1079c183ecf8d1
Sebastián Chavarry <69838707+sebastianperudev2001@users.noreply.github.com>
2021-12-01 21:11:20 -0500

    Commit Final
    Readme mejorado
    Código Final (o así espero)

--- Algoritmo.py
+++ Algoritmo.py
@@ -42,12 +42,19 @@ class Grafo:
         for i in range(self.cantV):
             print("{}\t\t\t{}".format(i, distancias[i]))
     
+    '''
+    Metodo auxiliar que permite generar el camino mas corto hacia cada nodo
+    Recibe como parametro la lista de predecesor, la cual contiene el orden 
+    de recorrido de los nodos
+    '''
+    
     def imprimirRutas(self, predecesor):        
         imprimir = ""     
         for nodo in range(self.cantV):
             current = nodo
             print("Ruta para llegar hasta el nodo {}".format(nodo))
             while(current != None):
+                #Se agrega una verificacion de formato para el primer caso 
                 if (imprimir == ""):
                     imprimir = imprimir + str(current)
                     current = predecesor[current]
@@ -57,11 +64,14 @@ class Grafo:
             print(imprimir)
             imprimir = ""
     
-    
+    '''
+    La funcion principal que nos permitira realizar la ejecucion del algoritmo
+    de la ruta mas corta
+    '''
     def BellmanFord(self, fuente):
         '''
         Paso 1: Inicializamos las distancias del nodo fuente a todos 
-        los vertices valore infinito
+        los vertices valore infinitos
         '''
         infinito = np.inf    
         distancias = [infinito] * self.cantV
@@ -69,21 +79,26 @@ class Grafo:
         predecesor = [None] * self.cantV
         
         '''
-        Paso 2: Aplicamos el relax V-1 a todos 
+        Paso 2: Aplicamos el relax V-1 a todos para hallar la ruta mas corta
         '''
+        #Se inicializa una variable auxiliar para controlar las iteraciones posibles
         iteracion = 0
         
+        #Se imprimer la tabla inicial antes de iterar 
         self.imprimir(distancias, fuente)
         
+        #Se actualiza el valor de distancia si la rota es la mas corta posible
         for nodo in range(self.cantV-1):
             for u,v,w in self.matriz: 
                 if  distancias[u] != infinito and distancias[u] + w < distancias[v]:
                     distancias[v] = distancias[u] + w
                     predecesor[v] = u
-                iteracion = iteracion + 1
+            iteracion = iteracion + 1
+            #Por cada nodo recorrido se va mostrando la tabla de distancias
             print('\n'+BLUE+BOLD +"Iteracion {}".format(iteracion) + '\n')
             self.imprimir(distancias, fuente)
-            
+        
+        #Se inicializa una variable que almacenara la cantidad total de iteraciones realizadas            
         global nro_repeticiones
         nro_repeticiones = iteracion 
         
@@ -98,15 +113,17 @@ class Grafo:
                 print("El Grafo contiene ciclos de peso negativo")
                 break    
         
+        #Se llama a la funcion auxiliar para imprimir las rutas generadas
         self.imprimirRutas(predecesor)
+        
+        #Se llama a la funcion auxiliar para imprimir la suma de pesos totales
         print(RED + BOLD + "\nALGORITMO BELLMAN-FORD")
         self.imprimir(distancias, fuente)
-       
-  
-
+        
 def main():   
-    #Leemos el archio txt que contiene las aristas en una lista
     opcion = ""
+    
+    #Se solicita al usuario que ingrese la topologia que desee experimentar
     while(True):
         try:
             num_temp = int(input("Ingrese el numero segun la topologia deseada: \n [1] \t Topologia de Complejidad Baja \n [2] \t Topologia de Complejidad Media \n"))
@@ -116,9 +133,12 @@ def main():
             elif (num_temp == 2):
                 opcion = 'topologia2.txt'
                 break
+            else:
+                print("INGRESE UN NUMERO VALIDO (1 o 2) \n")
         except ValueError:
                 print("INGRESE UN VALOR VALIDO (1 o 2) \n")
     
+    #Leemos el archivo txt que contiene las aristas en una lista
     try:
         with open(opcion, 'r') as f:
             temp = []
@@ -128,21 +148,46 @@ def main():
     except OSError:
         print("No se pudo abrir/leer el archivo: ", opcion)
         sys.exit()
+    
+    #La cantidad de nodos ya viene indicada en cada txt
+    try:
+        nodos= int(temp[0][0]) 
+    except IndexError as error:
+        print("{} es un error en la topografía dada. Revisar el archivo".format(error))
+        sys.exit()
+    except ValueError as mistake:
+        print("{} es un error en la topografía dada. Revisar el archivo".format(mistake))
+        sys.exit()
+
         
-    nodos= int(temp[0][0]) 
+    #Se solicita al usuario que ingrese el nodo origen para iniciar el recorrido
     while (True):
         try:
             fuente = int(input("Ingresa el nodo origen para aplicar el algoritmo: \n"))
-            break
+            if (fuente >= 0 and fuente < nodos):                
+                break
+            else:
+                print("{} no está dentro del rango 0 y {}".format(fuente, nodos-1))            
         except ValueError:
             print("Ingresar un numero entre 0 y {}".format(nodos-1))
+            
     #instanciamos el grafo
     g = Grafo(nodos)
     #Agregamos las aristas al grafo creado usando la lista anterior
     for i,line in enumerate(temp):
         if (i != 0): 
-            g.agregarArista(int(line[0]), int(line[1]), int(line[2]))           
+            try:
+                g.agregarArista(int(line[0]), int(line[1]), int(line[2]))  
+            except IndexError as error2:
+                print("{} es un error en la topografía dada. Revisar el archivo".format(error2))
+                sys.exit()
+            except ValueError as mistake2:
+                print("{} es un error en la topografía dada. Revisar el archivo".format(mistake2))
+                sys.exit()
+            
+    #Metricas
     
+    #Metodo para calcular la cantidad de segundos que se demora el algoritmo implementado
     execution_time = timeit.timeit(lambda: g.BellmanFord(fuente), number= 1)
     print(LIGHT_GREEN + "METRICAS")
     print("1) Tiempo de ejecucion (Segundos): ")
@@ -150,6 +195,7 @@ def main():
     #Metodo para calcular la cantidad de memoria usada
     print("2) Cantidad de memoria usada (MB): ")
     print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+    #Se imprimen la cantidad de iteraciones que tuvo que realizar el algoritmo
     print("3) Numero de iteraciones para hallar la ruta más corta")
     print(nro_repeticiones)
 
--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# ComputerNetworks-FinalProject-BellmanFord
- Experiment to analyse metrics of routing algorithms in two network scenarios
+# Redes de Computadoras: Proyecto Final-Bellman Ford
+
+
+
+![Logo de la Universidad de Lima](https://www.ulima.edu.pe/sites/all/themes/ulima2/logo.png)
+
+### Tabla de contenidos
+
+- [Descripción](#descripción)
+- [Instalación](#instalación)
+- [Licencia](#licencia)
+- [Información del autor](#información-del-autor)
+
+---
+
+## Descripción
+
+Este proyecto para el curso de Redes de Computadoras de la Universidad de Lima busca implementar el Algoritmo de Bellman-Ford en el contexto de enrutamiento de redes utilizando métricas de comparación en 2 topologías distintas.
+
+#### Tecnologías
+
+- Python
+- Github
+
+---
+
+## Instalación
+
+
+1. Clonar este repositorio en su computadora.
+
+![Imagen](descargar)
+
+2. Extraer el contenido en una carpeta.
+
+3. Abrir su terminal en dicho directorio y ejecutar el siguiente comando en su terminal o bash
+
+```html
+    python Algoritmo.py
+```
+Así en caso tenga Linux, MAC o el Bash de Git:
+
+![Imagen](Abrir-Terminal)
+
+Así en caso utilice Windows:
+
+![Imagen](cmd)
+
+En caso lo considere, también puede abrirlo desde su IDE de preferencia, en este caso se trabajó con Spyder, así que en caso solo debería seguir esta secuencia: 
+
+Projects > Open Project
+
+
+---
+
+## Licencia
+
+MIT License
+
+Copyright (c) [2017] [James Q Quick]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
+
+## Información del autor
+
+- Twitter - [@sbastianchvrryg](https://twitter.com/sbastianchvrryg)
+- LinkedIn - [Sebastián Chávarry](https://www.linkedin.com/in/sebasti%C3%A1n-ch%C3%A1varry-gutierrez/)
+
